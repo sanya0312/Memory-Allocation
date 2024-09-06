4b467dbce89a3b67c73c30164168c4cb213e53f4
Sanya0312 <sanyagandhi0312@gmail.com>
2024-09-06 21:58:23 +0530

    tests

--- test.c
+++ test.c
@@ -41,18 +41,12 @@ void Test2(){
     Process *p1 = createProcess(0, 500, 3);
     Process *p2 = createProcess(2, 200, 2);
     Process *p3 = createProcess(2, 150, 4);
-    Process *p4 = createProcess(4, 705, 2);
-    Process *p5 = createProcess(10, 500, 3);
-    Process *p6 = createProcess(12, 100, 2);
-    Process *p7 = createProcess(16, 1024, 1);
+    Process *p4 = createProcess(4, 705, 0);
 
     enqueueProcess(&queue, p1);
     enqueueProcess(&queue, p2);
     enqueueProcess(&queue, p3);
     enqueueProcess(&queue, p4);
-    enqueueProcess(&queue, p5);
-    enqueueProcess(&queue, p6);
-    enqueueProcess(&queue, p7);
 
     // Simulate processes
     printf("Starting simulation...\n");
